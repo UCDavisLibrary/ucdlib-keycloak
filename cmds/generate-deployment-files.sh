483047ae78af5a204b6bca6aee0b3b8258ccd8b0
spelkey-ucd <spelkey@ucdavis.edu>
2024-01-18 16:28:19 -0500

    start utils sidecar container

--- cmds/generate-deployment-files.sh
+++ cmds/generate-deployment-files.sh
@@ -32,3 +32,19 @@ for key in $(compgen -v); do
   content=$(echo "$content" | sed "s/{{$key}}/${escaped}/g")
 done
 echo "$content" > ../apache/keycloak.conf
+
+# generate local development dc file
+content=$(cat local-dev.yaml)
+LOCAL_BUILD=true source ../config.sh
+for key in $(compgen -v); do
+  if [[ $key == "COMP_WORDBREAKS" || $key == "content" || $key == "GOOGLE_KEY_FILE_CONTENT" ]]; then
+    continue;
+  fi
+  escaped=$(printf '%s\n' "${!key}" | sed -e 's/[\/&]/\\&/g')
+  content=$(echo "$content" | sed "s/{{$key}}/${escaped}/g")
+done
+if [ ! -d "../$LOCAL_DEV_DIRECTORY" ]; then
+  mkdir ../$LOCAL_DEV_DIRECTORY
+  touch ../$LOCAL_DEV_DIRECTORY/.env
+fi
+echo "$content" > ../$LOCAL_DEV_DIRECTORY/docker-compose.yaml
