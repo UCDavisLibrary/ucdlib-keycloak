e49031ba9d8b3175bbcc826afd700a0b39cc8875
spelkey-ucd <spelkey@ucdavis.edu>
2024-01-19 14:09:13 -0500

    init container

--- config.sh
+++ config.sh
@@ -4,9 +4,19 @@
 # Setup your application deployment here
 ########################################
 
+APP_SLUG=auth-kc
+
+# Grab build number is mounted in CI system
+if [[ -f /config/.buildenv ]]; then
+  source /config/.buildenv
+else
+  BUILD_NUM=-1
+fi
+
 # Main version number we are tagging the app with. Always update
 # this when you cut a new version of the app!
-APP_VERSION=v1.0.0
+APP_VERSION=v1.1.0.${BUILD_NUM}
+REPO_TAG=v1.1.0
 
 # Keycloak
 KC_TAG=21.1.2
@@ -20,6 +30,22 @@ POSTGRES_TAG=15.3
 
 # Utils
 NODE_TAG=20
+UTILS_TAG=v1.0.0
+BACKUP_FILE_NAME="db.sql.gz"
+GC_BACKUP_BUCKET="itis-iam/keycloak"
+
+# Custom images
+CONTAINER_REG_ORG=gcr.io/ucdlib-pubreg
+if [[ ! -z $LOCAL_BUILD ]]; then
+  CONTAINER_REG_ORG='localhost/local-dev'
+fi
+if [[ -z $UTILS_TAG ]]; then
+ CONTAINER_CACHE_TAG=$(git rev-parse --abbrev-ref HEAD)
+else
+ CONTAINER_CACHE_TAG=$UTILS_TAG
+fi
+UTILS_IMAGE_NAME=$CONTAINER_REG_ORG/$APP_SLUG-utils
+UTILS_IMAGE_NAME_TAG=$UTILS_IMAGE_NAME:$UTILS_TAG
 
 # local dev settings
 LOCAL_DEV_DIRECTORY=keycloak-local-dev
