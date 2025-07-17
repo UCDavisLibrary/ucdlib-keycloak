00fd303d846e2a97347aec1ddf6de138d30badf8
spelkey-ucd <spelkey@ucdavis.edu>
2025-07-17 17:00:30 -0400

    local dev #10

--- cmds/init-local-dev.sh
+++ cmds/init-local-dev.sh
@@ -8,12 +8,5 @@ set -e
 CMDS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $CMDS_DIR/..
 
-source ./config.sh
-
-if [ ! -d "./$LOCAL_DEV_DIRECTORY" ]; then
-  mkdir ./$LOCAL_DEV_DIRECTORY
-  touch ./$LOCAL_DEV_DIRECTORY/.env
-fi
-
-touch ./gc-writer-key.json
-touch ./gc-reader-key.json
+./cmds/get-reader-key.sh
+./cmds/get-env.sh local-dev
